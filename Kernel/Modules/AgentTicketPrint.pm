b43acb29c8ad555e6a92204f1f9a5768902660d4
Udo Bretz <udo.bretz@otrs.com>
2014-09-03 01:16:57 +0200

    Updated $origin commit id.

--- Kernel/Modules/AgentTicketPrint.pm
+++ Kernel/Modules/AgentTicketPrint.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketPrint.pm - print layout for agent interface
 # Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
 # --
-# $origin: https://github.com/OTRS/otrs/blob/72ee17c5fb32c7f225e319f77f4dbf4913613855/Kernel/Modules/AgentTicketPrint.pm
+# $origin: https://github.com/OTRS/otrs/blob/48a7c96de054455e3174837ea444d8fe0a767848/Kernel/Modules/AgentTicketPrint.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
