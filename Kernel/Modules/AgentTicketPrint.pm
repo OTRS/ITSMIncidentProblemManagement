105fcdda4b5bd9e9aea6cbb879c82a018d922c55
Stefan Bedorf <stefan.bedorf@otrs.com>
2016-01-03 19:24:02 +0100

    Updated copyright date.

--- Kernel/Modules/AgentTicketPrint.pm
+++ Kernel/Modules/AgentTicketPrint.pm
@@ -1,6 +1,6 @@
 # --
 # Kernel/Modules/AgentTicketPrint.pm - print layout for agent interface
-# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
 # $origin: https://github.com/OTRS/otrs/blob/4620110f5b36688dbae9c373a76a156a61d7a1d7/Kernel/Modules/AgentTicketPrint.pm
 # --
